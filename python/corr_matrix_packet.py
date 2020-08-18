8168f0a8d57d53bbe2d9803cd20c6d9bad50dae6
Isaac Dinis <isaac.dinis@epfl.ch>
2020-08-18 11:05:49 +0200

    added better comments

--- python/corr_matrix_packet.py
+++ python/corr_matrix_packet.py
@@ -14,21 +14,25 @@ import numpy as np
 logging.basicConfig(level=logging.ERROR)
 id = 'radio://0/80/2M'
 
+FFTSIZE = 32
+nMic = 4
+CRTP_PAYLOAD = 29
+N_BYTES = FFTSIZE*nMic*nMic*2*4 # *2 for complex numbers, *4 for bytes in float
+N_PACKETS,N_BYTES_LAST_PACKET = divmod(N_BYTES,CRTP_PAYLOAD) # N_PACKETS = number of packets -1
+
 class Audio_CRTP(object):
     """
-    Crazyflie console is used to receive characters printed using printf
-    from the firmware.
+    Audio CRTP recovers the correlation matrix through the CRTP protocol. The matrix is sent in 142 packets of 29 bytes.
+    The protocol has a start condition and then counts the incoming packets.
     """
 
     def __init__(self, crazyflie):
         """
-        Initialize the console and register it to receive data from the copter.
+        Initialisation
         """
-        self.corr_matrix = np.zeros((32,32))
-        self.packet_count = 141
-        self.packet_payload = 29
+        self.corr_matrix = np.zeros((FFTSIZE,nMic*nMic*2))
         self.receivedChar = Caller()
-        self.array = np.zeros(4096)
+        self.array = np.zeros(N_BYTES)
         self.start = 0
         self.index = 0
         self.start_time = 0
@@ -39,33 +43,30 @@ class Audio_CRTP(object):
         """
         Callback for data received from the copter.
         """
-        # This might be done prettier ;-)
-        receive_byte = packet.data
-        if packet.channel == 1:
+        if packet.channel == 1: # Channel 1 is used for start condition
             if packet.datal[0] == 0:
-                if self.index != 0 and self.index != self.packet_count+1:
+                if self.index != 0 and self.index != N_PACKETS+1: # n packets received != expectation
                     print("packets loss")
-                self.index = 0
+                self.index = 0 # reset index
                 self.start = 1
                 self.start_time = time.time()
         elif self.start:
-            if self.index == self.packet_count:
-                self.array[self.index * self.packet_payload:self.index * self.packet_payload + 7] = packet.datal[0:7]
-                self.array.astype(np.uint8).tofile("bin")
-                self.corr_matrix = np.fromfile("bin",np.float32).reshape((32,32))
+            if self.index == N_PACKETS:
+                self.array[self.index * CRTP_PAYLOAD:self.index * CRTP_PAYLOAD + N_BYTES_LAST_PACKET] = \
+                    packet.datal[0:N_BYTES_LAST_PACKET] # last bytes
+                self.array.astype(np.uint8).tofile("bin") # write array to dummy bin file to convert uint8 to float32
+                self.corr_matrix = np.fromfile("bin",np.float32).reshape((FFTSIZE,nMic*nMic*2))
                 print("Elapsed time = {}s ".format(time.time()-self.start_time))
             else:
-                self.array[self.index*self.packet_payload:(self.index+1)*self.packet_payload] = packet.datal
+                self.array[self.index*CRTP_PAYLOAD:(self.index+1)*CRTP_PAYLOAD] = packet.datal
             self.index += 1
 
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers(enable_debug_driver=False)
-
     with SyncCrazyflie(id) as scf:
-        time.sleep(5)
         cf = scf.cf
-        audio_crtp = Audio_CRTP(cf)
+        audio_CRTP = Audio_CRTP(cf)
         while True:
             time.sleep(1)
 
