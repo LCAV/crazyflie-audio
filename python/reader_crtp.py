ca746b88b0df84858167d7c0a6fb401ef78d2708
duembgen <frederike.duembgen@epfl.ch>
2020-08-25 09:44:28 +0200

    Refactor CRTP reading (#1)
    * Start refactoring
    * Rename CRTP reader
    * Finish motion reading

---
+++ python/reader_crtp.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+import time
+
+import numpy as np
+
+from cflib.utils.callbacks import Caller
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.crazyflie.syncLogger import SyncLogger
+import cflib.crtp
+
+# TODO(FD) for now, below only works with modified cflib.
+# from cflib.crtp.crtpstack import CRTPPort
+# CRTP_PORT_AUDIO = CRTPPort.AUDIO
+
+CRTP_PORT_AUDIO = 0x09
+
+# Only output errors from the logging framework
+logging.basicConfig(level=logging.ERROR)
+id = "radio://0/80/2M"
+
+FFTSIZE = 32
+N_MICS = 4
+CRTP_PAYLOAD = 29 # number of bytes per package
+FLOAT_PRECISION = 4 # number of bytes for float32
+N_FLOATS = FFTSIZE * N_MICS * 2  # *2 for complex numbers
+N_BYTES = N_FLOATS * FLOAT_PRECISION
+N_FULL_PACKETS, N_BYTES_LAST_PACKET = divmod(N_BYTES, CRTP_PAYLOAD)
+
+def set_thrust(cf,thrust):
+    thrust_str = f'{thrust}'
+    cf.param.set_value('motorPowerSet.m4', thrust_str)
+    cf.param.set_value('motorPowerSet.m1', thrust_str)
+    cf.param.set_value('motorPowerSet.m2', thrust_str)
+    cf.param.set_value('motorPowerSet.m3', thrust_str)
+    cf.param.set_value('motorPowerSet.enable', '1')
+
+class ReaderCRTP(object):
+    """
+    ReaderCRTP recovers the data sent through the CRTP protocol and publishes them. 
+    There are different schemes for different data types:
+
+    - audio_data: 
+    The audio data (the FFT signals at N_FREQUENCY bins, recorded from N_MICS microphones) is sent in packets of CRTP_PAYLOAD bytes each.
+    The new data frame starts when the start condition is met(channel==1) and we count the incoming packets to make sure there is no packet loss.
+
+    - motion_data: 
+    We read the current motion estimate through the standard logging framework provided by the Crazyflie, and then publish the estimate as a Pose.
+    """
+    def __init__(self, crazyflie, verbose=False):
+        self.array = np.zeros(N_BYTES, dtype=np.uint8)
+
+        self.receivedChar = Caller()
+        self.start = False
+        self.index = 0
+        self.start_time = 0
+        self.cf = crazyflie
+        self.verbose = verbose
+
+        # TODO(FD): figure out if below changes when the Crazyflie actually flies.
+        # Tests have shown that when the flowdeck is attached to the Crazyflie and 
+        # it is moved around (without flying), then 
+        # - stabilizer.yaw, controller.yaw and stateEstimate.yaw are all the same. 
+        #   They are in degrees and clipped to -180, 180.
+        # - mag.x sometimes gives values similar to above 3, 
+        #   but sometimes it is constantly zero (should it only be used outside? 
+        # - gyro.z gives the raw yaw rate (or acceleration?).
+        # - motion.deltaX and motion.deltaY can be very noisy, especially when 
+        #   the ground is not textured. 
+        # - stabilizer.vx and stabilizer.vy are more stable but of course would need 
+        #   to be integrated for a position estimate.
+        lg_motion = LogConfig(name='Motion2D', period_in_ms=300)
+        lg_motion.add_variable('stabilizer.yaw', 'float')
+        lg_motion.add_variable('motion.deltaX', 'float')
+        lg_motion.add_variable('motion.deltaY', 'float')
+        self.cf.log.add_config(lg_motion)
+        lg_motion.data_received_cb.add_callback(self.callback_logging)
+        lg_motion.start()
+
+        self.cf.add_port_callback(CRTP_PORT_AUDIO, self.callback_audio)
+
+        # this data can be read and published by ROS nodes
+        self.audio_data = {'time': None, 'data': None, 'published': True}
+        self.motion_data = {'time': None, 'data': None, 'published': True}
+
+    def callback_audio(self, packet):
+        # We send the first package in channel 1 to identify the start of new audio data.
+        if packet.channel == 1:
+            if (self.index != 0) and (self.index != N_FULL_PACKETS + 1):
+                print(f"packets loss: received only {self.index}/{N_FULL_PACKETS+1}")
+            self.index = 0  # reset index
+            self.start = True
+            self.start_time = time.time()
+
+        if self.start:
+            # received all full packets, read remaining bytes
+            if self.index == N_FULL_PACKETS:
+                self.array[
+                    self.index * CRTP_PAYLOAD : self.index * CRTP_PAYLOAD
+                    + N_BYTES_LAST_PACKET
+                ] = packet.datal[
+                    0:N_BYTES_LAST_PACKET
+                ]  # last bytes
+                
+                self.audio_data['data'] = np.frombuffer(self.array, dtype=np.float32)
+                self.audio_data['time'] = time.time()
+                self.audio_data['published'] = False
+
+                if self.verbose:
+                    packet_time = self.audio_data['time'] - self.start_time
+                    print(f"callback_audio: time for all packets: {packet_time}s")
+            else:
+                self.array[
+                    self.index * CRTP_PAYLOAD : (self.index + 1) * CRTP_PAYLOAD
+                ] = packet.datal # packet in list format
+            self.index += 1
+
+    def callback_logging(self, timestamp, data, logconf):
+        if self.verbose:
+            print('callback', timestamp, data, logconf.name)
+
+        # TODO(FD): figure out if this timestamp is correct.
+        self.motion_data['time'] = timestamp
+        self.motion_data['published'] = False
+        self.motion_data['data'] = {
+            'yaw': data['stabilizer.yaw'], 
+            'dx': data['motion.deltaX'],
+            'dy': data['motion.deltaY']
+        }
+
+if __name__ == "__main__":
+    verbose = True
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+
+    with SyncCrazyflie(id) as scf:
+        cf = scf.cf
+        #set_thrust(cf, 43000)
+        reader_crtp = ReaderCRTP(cf, verbose=verbose)
+
+        while True:
+            time.sleep(1)
